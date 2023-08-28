9c86ab27e448dc583751ab95ecd872d917bb42b4
Nana Awuku <apatrickadjei@gmail.com>
2023-08-28 07:35:53 +0000

    nw commit

--- src/App.js
+++ src/App.js
@@ -33,89 +33,105 @@ function App() {
     : blockInfo?.transactions.slice(0, 5);
 
   return (
-    <div className="w-full bg-blue-50 h-screen p-4 flex">
-      <div className="w-1/2 pr-4">
-        {error && <div className="text-red-500">{error.message}</div>}
-        {blockInfo && (
-          <div className="mb-4">
-            <h2 className="mb-2 text-1xl text-left capitalize text-blue-800">
-              Latest Block Information
-            </h2>
-            <div className="bg-white h-auto p-4 divide-y divide-blue-200">
-              <div className="py-3 flex justify-between items-center">
-                <span className="text-[12px] font-semibold">Block Number:</span>
-                <span className="text-[12px]">{blockInfo.number}</span>
-              </div>
-              <div className="py-3 flex justify-between items-center">
-                <span className="text-[12px] font-semibold">Block Hash:</span>
-                <span className="text-[12px]">{blockInfo.hash.substring(0, 40)}...</span>
-              </div>
-              <div className="py-3 flex justify-between items-center">
-                <span className="text-[12px] font-semibold">Miner:</span>
-                <span className="text-[12px]">{blockInfo.miner.substring(0, 40)}...</span>
-              </div>
-              <div className="py-3 flex justify-between items-center">
-                <span className="text-[12px] font-semibold">Transaction Count:</span>
-                <span className="text-[12px]">{blockInfo.transactions.length}</span>
-              </div>
-              <div className="py-3 flex justify-between items-center">
-                <span className="text-[12px] font-semibold">Timestamp:</span>
-                <span className="text-[12px]">{blockInfo.timestamp}</span>
+    <div className="w-full bg-slate-50 h-full p-4">
+      <h2 className="uppercase text-2xl text-center">Block Explorer</h2>
+      <div className="flex">
+        <div className="w-1/2 pr-4">
+          {error && <div className="text-red-500">{error.message}</div>}
+          {blockInfo && (
+            <div className="mb-4">
+              <h2 className="mb-2 text-1xl text-left capitalize text-blue-800">
+                Latest Block Information
+              </h2>
+              <div className="bg-white h-auto p-4 divide-y divide-blue-200">
+                <div className="py-3 flex justify-between items-center">
+                  <span className="text-[12px] font-semibold">
+                    Block Number:
+                  </span>
+                  <span className="text-[12px]">{blockInfo.number}</span>
+                </div>
+                <div className="py-3 flex justify-between items-center">
+                  <span className="text-[12px] font-semibold">Block Hash:</span>
+                  <span className="text-[12px]">
+                    {blockInfo.hash.substring(0, 40)}...
+                  </span>
+                </div>
+                <div className="py-3 flex justify-between items-center">
+                  <span className="text-[12px] font-semibold">Miner:</span>
+                  <span className="text-[12px]">
+                    {blockInfo.miner.substring(0, 40)}...
+                  </span>
+                </div>
+                <div className="py-3 flex justify-between items-center">
+                  <span className="text-[12px] font-semibold">
+                    Transaction Count:
+                  </span>
+                  <span className="text-[12px]">
+                    {blockInfo.transactions.length}
+                  </span>
+                </div>
+                <div className="py-3 flex justify-between items-center">
+                  <span className="text-[12px] font-semibold">Timestamp:</span>
+                  <span className="text-[12px]">{blockInfo.timestamp}</span>
+                </div>
               </div>
             </div>
-          </div>
-        )}
-      </div>
-      <div className="w-1/2 pl-4">
-        {blockInfo && (
-          <div>
-            <h3 className="mb-2 text-right capitalize text-blue-800">
-              Transaction List
-            </h3>
-            <div className="divide-y divide-blue-200 h-screen bg-white">
-              {displayedTransactions &&
-                displayedTransactions.map((tx, index) => (
-                  <div key={index} className="py-1 px-3 bg-white">
-                    <div className="flex justify-between items-center">
-                      <span className="flex items-center gap-3">
-                        <span className="bg-slate-200 p-2 rounded-full text-[10px]">
-                          TX
-                        </span>
-                        <span className="text-[12px]">
-                          {tx.hash.substring(0, 8)}...{" "}
-                        </span>
-                      </span>
-                      <span className="flex flex-col">
-                        <div>
-                          <span className="text-[12px]">From:</span>
-                          <span className="text-[12px]">
-                            {tx.from.substring(0, 20)}...
+          )}
+        </div>
+        <div className="w-1/2 pl-4">
+          {blockInfo && (
+            <div>
+              <h3 className="mb-2 text-right capitalize text-blue-800">
+                Transaction List
+              </h3>
+              <div className="divide-y divide-blue-200 h-full pb-4 bg-white">
+                {displayedTransactions &&
+                  displayedTransactions.map((tx, index) => (
+                    <div key={index} className="py-1 px-3 bg-white">
+                      <div className="flex justify-between items-center">
+                        <span className="flex items-center gap-3">
+                          <span className="bg-slate-200 p-2 rounded-full text-[10px]">
+                            TX
                           </span>
-                        </div>
-                        <div>
-                          <span className="text-[12px]">To:</span>{" "}
                           <span className="text-[12px]">
-                            {tx.to.substring(0, 20)}...
+                            {tx.hash.substring(0, 8)}...{" "}
                           </span>
-                        </div>
-                      </span>
-                      <span className="text-[12px] bg-slate-200 p-1">
-                        {tx.value.toString().substring(0, 5)}
-                      </span>
+                        </span>
+                        <span className="flex flex-col">
+                          <div>
+                            <span className="text-[12px]">From:</span>
+                            <span className="text-[12px]">
+                              {tx.from.substring(0, 20)}...
+                            </span>
+                          </div>
+                          <div>
+                            <span className="text-[12px]">To:</span>{" "}
+                            <span className="text-[12px]">
+                              {tx.to.substring(0, 20)}...
+                            </span>
+                          </div>
+                        </span>
+                        <span className="text-[12px] bg-slate-200 p-1 px-2 rounded-md">
+                          {tx.value.toString().substring(0, 5)}
+                        </span>
+                      </div>
                     </div>
-                  </div>
-                ))}
-              {!showAllTransactions && (
-                <button
-                  className="mt-4 py-1 px-4 bg-blue-500 w-full text-white rounded-md"
-                  onClick={() => setShowAllTransactions(true)}
-                >
-                  Load All Transactions
-                </button>
-              )}
+                  ))}
+                
+                <span className="flex items-center justify-center">
+                {!showAllTransactions && (
+                  <button
+                    className="mt-4 py-1 px-4 bg-slate-200 rounded-md"
+                    onClick={() => setShowAllTransactions(true)}
+                  >
+                    Load All Transactions
+                  </button>
+                )}
+                </span>
+              </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </div>
   );
